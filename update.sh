81dff63a160bf84c33479c40652f8a1abb24b0da
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2025-12-16 21:35:22 -0500

    Fix update script
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- update.sh
+++ update.sh
@@ -4,4 +4,5 @@ moon add moonbitlang/async
 moon add moonbitlang/x
 moon add TheWaWaR/clap
 moon add gmlewis/fonts
+pushd examples/03-engraved-name-tag/ && moon update && moon install && rm -rf target && popd
 ./test-all.sh
