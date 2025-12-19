6d9d68abfd720c8566939e00be615bd12836db49
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2025-12-18 19:36:35 -0500

    Fix update.sh
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- examples/03-engraved-name-tag/update.sh
+++ examples/03-engraved-name-tag/update.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -ex
 find . -depth -name target -type d -exec rm -rf {} \;
 find . -depth -name .mooncakes -type d -exec rm -rf {} \;
-sed -i '/moonbitlang\/async/d' moon.mod.json
-sed -i '/moonbitlang\/x/d' moon.mod.json
-sed -i '/TheWaWaR\/clap/d' moon.mod.json
-sed -i '/gmlewis\/fonts/d' moon.mod.json
-sed -i '/gmlewis\/fonts-b/d' moon.mod.json
+sed "/moonbitlang\/async/d" moon.mod.json > moon.mod.json.tmp && mv moon.mod.json.tmp moon.mod.json
+sed "/moonbitlang\/x/d" moon.mod.json > moon.mod.json.tmp && mv moon.mod.json.tmp moon.mod.json
+sed "/TheWaWaR\/clap/d" moon.mod.json > moon.mod.json.tmp && mv moon.mod.json.tmp moon.mod.json
+sed "/gmlewis\/fonts/d" moon.mod.json > moon.mod.json.tmp && mv moon.mod.json.tmp moon.mod.json
+sed "/gmlewis\/fonts-b/d" moon.mod.json > moon.mod.json.tmp && mv moon.mod.json.tmp moon.mod.json
 moon add moonbitlang/async
 moon add moonbitlang/x
 moon add TheWaWaR/clap
