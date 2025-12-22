d248078e5c7c7197a9e9a76e4af221f3359f6cd0
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2025-12-22 18:05:00 -0500

    Add next entity
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- test-all.sh
+++ test-all.sh
@@ -4,12 +4,12 @@ moon fmt
 moon info --target native
 moon test --target native
 
-# Test subdirs that have their own moon.mod.json
-DIRS_NEEDING_TESTING=$(find examples -name target -prune -o -name .mooncakes -prune -o -type f -name "moon.mod.json" -exec dirname {} \;)
-for dir in $DIRS_NEEDING_TESTING; do
+# Check subdirs that have their own moon.mod.json
+DIRS_NEEDING_CHECKING=$(find examples -name target -prune -o -name .mooncakes -prune -o -type f -name "moon.mod.json" -exec dirname {} \;)
+for dir in $DIRS_NEEDING_CHECKING; do
 	pushd "$dir" \
 		&& moon fmt \
 		&& moon info --target native \
-		&& moon test --target native \
+		&& moon check --target native \
 		&& popd
 done
