e71176d66be44a0aa204b8ee6c259c5602366f76
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2025-12-22 12:09:09 -0500

    Update examples
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- update.sh
+++ update.sh
@@ -6,7 +6,7 @@ moon add moonbitlang/x
 moon add TheWaWaR/clap
 moon add gmlewis/fonts
 ./scripts/sync-fonts-versions.py
-DIRS_NEEDING_UPDATE=$(find examples -type f -name "moon.mod.json" -exec dirname {} \;)
+DIRS_NEEDING_UPDATE=$(find examples -name target -prune -o -name .mooncakes -prune -o -type f -name "moon.mod.json" -exec dirname {} \;)
 for dir in $DIRS_NEEDING_UPDATE; do
   pushd "$dir"
   ./update.sh
