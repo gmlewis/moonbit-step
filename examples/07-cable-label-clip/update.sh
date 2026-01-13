2be57ba16ee36d778fee51ff4bdfcbd9b1db7822
Glenn Lewis <6598971+gmlewis@users.noreply.github.com>
2026-01-13 10:48:25 -0500

    Use gmlewis/fonts/loader
    Signed-off-by: Glenn Lewis <6598971+gmlewis@users.noreply.github.com>

--- examples/07-cable-label-clip/update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -ex
-moon add --no-update moonbitlang/async
-moon add --no-update moonbitlang/x
-moon add --no-update TheWaWaR/clap
-moon add --no-update gmlewis/fonts
-moon add --no-update gmlewis/fonts-b
-moon install
-moon check --target native
--- test-all.sh
+++ test-all.sh
@@ -3,13 +3,3 @@ export NEW_MOON=1
 moon fmt
 moon info --target native
 moon test --target native
-
-# Check subdirs that have their own moon.mod.json
-DIRS_NEEDING_CHECKING=$(find examples -name target -prune -o -name .mooncakes -prune -o -type f -name "moon.mod.json" -exec dirname {} \;)
-for dir in $DIRS_NEEDING_CHECKING; do
-	pushd "$dir" \
-		&& moon fmt \
-		&& moon info --target native \
-		&& moon check --target native \
-		&& popd
-done
